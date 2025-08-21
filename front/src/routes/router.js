a0b2223eda208330c97f8b4684faaf1135a7bf23
Maria De Los Angeles <academicmarmolcmaria@gmail.com>
2025-08-21 09:22:50 -0500

    changes applied

--- front/src/routes/router.js
+++ front/src/routes/router.js
@@ -2,12 +2,14 @@
 import Admin from "../views/admin.js";
 import NotFound from "../views/notFound.js";
 import Home from "../views/home.js";
+import Contestant from "../views/contestant.js"; 
 import AccessDenied from "../views/accessDenied.js";
 
 // definicion de rutas disponibles en la aplicacion
 const routes = {
   "/": { view: Home },
   "/Admin": { view: Admin },
+  "/Contestant" : { view: Contestant },
 };
 
 export function router() {

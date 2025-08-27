6386df5ae5fa84864aaa9ba1d56252fed0a1ee66
diegofelipe00riwi <diegofelipe2915@gmail.com>
2025-08-27 09:08:36 -0500

    update login

--- front/src/views/login.js
+++ front/src/views/login.js
@@ -1,47 +1,81 @@
 export default function login() {
-  return`
-     <form id="login-form">
-    <div class="text-center mb-4">
-        <img src="../../../public/images/RecreasportLOGO.png" alt="RecreaSport Logo" class="img-fluid" style="max-height: 100px;">
-    </div>
-    
-    <h2 class="text-center mb-4">Login</h2>
+  return `
 
-    <div class="mb-3">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="login-username" placeholder="Username" required>
-        <label for="login-username">Username</label>
-      </div>
-    </div>
+<form id="login-form">
 
-    <div class="mb-3">
-      <div class="form-floating">
-        <input type="password" class="form-control" id="login-password" placeholder="Password" required>
-        <label for="login-password">Password</label>
-      </div>
-    </div>
+ <div class="text-center mb-4">
 
-    <div class="d-flex justify-content-between align-items-center mb-3">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="remember-me">
-        <label class="form-check-label" for="remember-me">
-          Remember me
-        </label>
-      </div>
-      <a href="#">Forgot Password?</a>
-    </div>
+<img src="./public/images/RecreasportLOGO.png" alt="RecreaSport Logo" class="img-fluid" style="max-height: 100px;">
 
-    <button type="submit" class="btn btn-primary w-100 mb-3">Login</button>
+</div>
 
-    <div class="d-grid mb-3">
-      <button type="button" id="btnGoogleLogin" class="btn btn-outline-secondary">
-        <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" style="width: 20px;" class="me-2">
-        Sign in with Google
-      </button>
-    </div>
+<h2 class="text-center mb-4">Login</h2>
 
-    <div class="text-center">
+
+
+
+
+<div class="mb-3" style="max-width: 400px; margin: auto; ">
+
+<label for="login-username" class="form-label">Username</label>
+
+<input type="text" class="form-control" id="login-username" placeholder="" required>
+
+</div>
+
+
+
+<div class="mb-4" style="max-width: 400px; margin: auto; ">
+
+<label for="login-password" class="form-label">Password</label>
+
+<input type="password" class="form-control" id="login-password" placeholder="" required>
+
+</div>
+
+
+
+<div class="d-flex justify-content-between align-items-center mb-3" style="max-width: 500px; margin: auto;">
+
+<div class="form-check">
+
+<input class="form-check-input" type="checkbox" id="remember-me">
+
+<label class="form-check-label" for="remember-me">
+  Remember me 
+</label>
+
+</div>
+
+<a href="#">Forgot Password?</a>
+
+</div>
+
+<div class="text-center mb-3" style="max-width: 500px; margin: auto;">
+
+<button type="submit" class="btn btn-primary w-100 mb-3" style="max-width: 500px; margin auto;" >Login</button>
+</div>
+
+
+<div class="d-grid mb-3" style="max-width: 500px; margin: auto;">
+
+<button type="button" id="btnGoogleLogin" class="btn btn-outline-secondary">
+
+<img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" style="width: 20px;" class="me-2">
+
+Sign in with Google
+
+</button>
+
+</div>
+
+ <div class="text-center">
+
       <p>Don't have an account? <a href="/register">Register</a></p>
-    </div>
-  </form>
-`};
+
+  </div>
+
+</form>
+
+`;
+}

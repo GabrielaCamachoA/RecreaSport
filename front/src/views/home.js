8ca549f0f48025783bd3ee1bc3cc7ed17c51110c
OCP3X <ocp3x@hotmail.com>
2025-08-28 07:00:38 -0500

    update files

--- front/src/views/home.js
+++ front/src/views/home.js
@@ -1,3 +1,60 @@
 export default function Home() {
-  return `<h1 class="h1">Home</h1>`;
+  return `
+  <section class="section-bg">
+    <h2>Programa Deporte Recreativo </h2>
+    <p>Barranquilla 2025</p>
+    <p>Plataforma integral para la gestión de inscripciones y seguimiento del programa deportivo recreativo de la Alcaldía de Barranquilla.</p>
+     <div class="btn">
+      <a href="/login" data-link id="btnLogin"> lniciar Sesion </a>
+      <a href="/register" data-link id="btnRegister"> lnscríbete Ahora </a>
+    </div>
+  </section>
+  <section class="section-deports">
+    <p>Deportes </p>
+    <h2>Disciplinas Deportivas</h2>
+    <p>Descubre todas las modalidades deportivas disponibles en el programa y únete a la que más te apasione</p>
+     <div class="cards">
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+      <div class="tarjet">
+       <img src="../public/images/RecreasportLOGO.png" alt="logo-RecreaSport" class="img-fluid">
+       <pan>Fútbol</pan>
+       <p>200 inscritos</p>
+      </div>
+    </div>
+  </section>
+  `;
 }
